4c31fcff2cf9116023594d83718f7b10136f4e83
kevin-montrose <kevin.montrose@gmail.com>
2015-10-27 14:56:58 -0400

    missing file

---
+++ CommonMark.Tests/TableTests.cs
@@ -0,0 +1,36 @@
+﻿using CommonMark.Syntax;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace CommonMark.Tests
+{
+    [TestClass]
+    public class TableTests
+    {
+        static CommonMarkSettings Settings;
+
+        static TableTests()
+        {
+            Settings = CommonMarkSettings.Default.Clone();
+            Settings.AdditionalFeatures = CommonMarkAdditionalFeatures.GithubStyleTables;
+            Settings.TrackSourcePosition = true;
+        }
+
+        [TestMethod]
+        public void SimpleTable()
+        {
+            var ast = 
+                CommonMarkConverter.Parse(
+@"First Header  | Second Header
+------------- | -------------
+Content Cell  | Content Cell
+Content Cell  | Content Cell",
+                    Settings
+                );
+
+            var firstChild = ast.FirstChild;
+            Assert.AreEqual(BlockTag.Table, firstChild.Tag);
+        }
+    }
+}
