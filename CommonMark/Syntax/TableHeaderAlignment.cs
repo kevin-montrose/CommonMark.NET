8ebb2dc1c2e937f12d855ef5070011e40b251e1a
kevin-montrose <kevin.montrose@gmail.com>
2015-10-27 20:33:34 -0400

    missing file

---
+++ CommonMark/Syntax/TableHeaderAlignment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonMark.Syntax
+{
+    /// <summary>
+    /// Defines the alignment specified in a header column for a GithubStyleTable
+    /// </summary>
+    public enum TableHeaderAlignment
+    {
+        /// <summary>
+        /// No alignment specified
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Left alignment
+        /// </summary>
+        Left = 1,
+
+        /// <summary>
+        /// Right alignment
+        /// </summary>
+        Right = 2,
+
+        /// <summary>
+        /// Center alignment
+        /// </summary>
+        Center = 3
+    }
+}
